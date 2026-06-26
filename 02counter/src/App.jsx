580d7763b205c0016d0b2f446c2431fb21540954
Bharat Malani <malanibharat11@gmail.com>
2026-06-26 20:58:06 +0530

    new add

---
+++ 02counter/src/App.jsx
@@ -0,0 +1,39 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+
+function App() {
+
+  let [counter, setCounter] = useState(5)
+
+  // let counter = 5
+  const addValue = () => {
+    let cnt = counter
+    if(cnt <10) {
+    // counter = counter + 1
+      setCounter(counter + 1)
+    }
+  }
+
+  const removeVlaue = () => {
+    let cnt = counter
+    if(cnt > 0) {
+      setCounter(counter - 1)
+    }
+  }
+
+  return (
+    <>
+      <h1>Chai aur react</h1>
+      <h2>Counter Value : {counter}</h2>
+
+      <button onClick={addValue}>Add Value</button>
+      <br />
+      <button onClick={removeVlaue}>remove value</button> 
+    </>
+  )
+}
+
+export default App
---
+++ chai aur react/src/main.jsx
@@ -0,0 +1,17 @@
+// import { StrictMode } from 'react'
+import React from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App.jsx'
+
+const reactElement = React.createElement(
+  'a',
+  {
+    href: 'https://www.google.com',target: '_blank'
+  },
+  'Click me to visit google'
+)
+
+createRoot(document.getElementById('root')).render(
+    <App />
+    // reactElement
+)
